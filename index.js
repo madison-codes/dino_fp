4cdba3c845700f069e8f64053449417f83d94b6c
madison-kerndt <madison.kerndt@gmail.com>
2018-06-15 08:51:06 -0600

    Add add one and map example in both a functional implementation and not

--- index.js
+++ index.js
@@ -1,41 +1,42 @@
-   addOne = () => {
-        const x = 1;
-        x = x + 1
+const x = 1;
+
+/**
+ * This is not a functional function because we are manipulating a global variable.
+ */
+function addOne() {
+    x = x + 1
+}
+
+/**
+ * This is a functional implementation of the function above. `X` remains the same and we return a new variable with a new value.
+ */
+function addOneFunctional(x) {
+    const z = x + 1;
+    return z;
+}
+
+/**
+ * Iterate through every value in a collection and add one to it. This manipulates the origional array.
+ */
+const array = [1, 2, 3, 4, 5];
+
+function addOneToCollection(array) {
+    for (i = 0; i > array.length; i++) {
+        return array[i] + 1;
     }
-    // x = 1
+}
 
-    return2 = () => {
-        const x = 1;
-        const y = x + 1;
-        return y;
-    }
-    // x = 1
-    // y = 2
+/**
+ * Iterate through every value in a collection and add one to it. Map returns a new array and does not change the origional array.
+ */
+const array = [1, 2, 3, 4, 5];
 
-    add = (x: number, y: number) => {
-        return x + y;
-    }
+function addOneToCollection(array) {
+    array.map((value) => {
+        return value + 1;
+    });
+}
 
-    mappable = (arr: number[]) => {
-        arr.map((numberValue) => {
-            return numberValue + 2;
-        });
-    }
+// [EMILY] Loop through a collection of numbers
 
-    recusrsion = () => {
-        recusrsion()
-    }
-
-    const arr = [1, 2, 3];
-    add(arr);
-    mappable(arr);
-    recusrsion(arr);
-
-    add((arr) => mappable(arr));
-
-// arr [1, 2, 3]
-// mappable(arr) [3, 4, 5]
-
-// x = 1
-// y = 1
-// z = 2
+// [EMILY] Recursive loop through a collection of numbers
